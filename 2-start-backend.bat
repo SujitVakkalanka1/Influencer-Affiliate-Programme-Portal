efb054af0e63d1f0dfd5f830a0a0a4bc96a5cc5d
SujitVakkalanka1 <vsbsujit883@gmail.com>
2026-06-15 03:49:41 +0000

    delete

--- 2-start-backend.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd backend
-echo Starting backend on http://localhost:5000
-echo Backend will use MongoDB from backend\.env MONGO_URI.
-echo If this fails, install MongoDB Community Server or use MongoDB Atlas.
-call npm run dev
-pause
